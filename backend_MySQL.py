6bd38a2bdb1b1eef7a057df6b903cd3c868e147e
Jerophin D R <151863386+Jerophin123@users.noreply.github.com>
2025-09-05 20:48:37 +0530

    comm

--- backend_MySQL.py
+++ backend_MySQL.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
-from keras.models import load_model
-from keras.layers import DepthwiseConv2D
+try:
+    from tensorflow.keras.models import load_model
+    from tensorflow.keras.layers import DepthwiseConv2D
+except ImportError:
+    from keras.models import load_model
+    from keras.layers import DepthwiseConv2D
 from PIL import Image, ImageOps
 import numpy as np
 import os
